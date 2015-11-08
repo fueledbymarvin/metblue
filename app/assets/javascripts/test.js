b94e372fe9c49ddbb2ce3a33ceef89d2bf80b6ba
dhpark <dhpark01@email.wm.edu>
2015-11-08 06:55:33 -0500

    have horizontal scroling

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 300) {
+    $("#makeMeScrollable").smoothDivScroll({
+      mousewheelScrolling: "allDirections",
+      manualContinuousScrolling: true,
+      autoScrollingMode: "onStart"
+    });
 
+    $(window).scroll(function() {
+        if ($(window).scrollLeft() > 300) {
+            alert("LOL");
           //get data//
-          $.get('/api/users/563ec9ee853d9855d5000000/search/packages',
-          {page: page_count, per_page:5 }).done(function(response){
-            console.log(response);
-          })
-            $(window).scrollTop(0);
+          // $.get('/api/users/563ec9ee853d9855d5000000/search/packages',
+          // {page: page_count, per_page:5 }).done(function(response){
+          //   console.log(response);
+          // })
+            $(window).scrollLeft(0);
         }
     });
 });
