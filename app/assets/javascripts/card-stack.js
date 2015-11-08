195fde169149ca0bf52b9efeef0b48587784264d
Amanda Liu <ajliu@mit.edu>
2015-11-08 01:59:16 -0500

    Added dummy data requests for testing...

--- app/assets/javascripts/card-stack.js
+++ app/assets/javascripts/card-stack.js
@@ -56,7 +56,24 @@ $(document).ready(function(){
     drop: function(event, ui){
       $("#favorites").append(ui.draggable);
       ui.draggable.removeAttr("style");
+      ui.draggable.addClass("favoriteCard");
     }
     //tolerance:"touch"
   });
+  $('#favorites-wrapper').click(function(){
+    alert("clicked");
+    $.ajax({
+      url:'/api/users/563ec9ee853d9855d5000000/search/',
+      type: 'PUT',
+      data: {search: {origin_airport: 'BOS'}}
+    }).done(function(response){
+      console.log("success?");
+      console.log(response);
+      //true
+    }).fail(function(responseObject){
+      console.log("error :(");
+      console.log(responseObject);
+    });
+  })
+
 });
--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         if ($(window).scrollTop() > 300) {
-            alert("new deal!");
+
+          //get data//
+          $.get('/api/users/563ec9ee853d9855d5000000/search',
+          {search: {budget: 700}}).done(function(response){
+            console.log(response);
+          })
             $(window).scrollTop(0);
         }
     });
