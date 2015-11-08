31bde09e6087dc9bd017d97836d9712da7896d74
Amanda Liu <ajliu@mit.edu>
2015-11-08 01:59:46 -0500

    ...actually saved my changes. dummy requests.

--- app/assets/javascripts/card-stack.js
+++ app/assets/javascripts/card-stack.js
@@ -70,10 +70,14 @@ $(document).ready(function(){
       console.log("success?");
       console.log(response);
       //true
+      $.get(
+        '/api/users/563ec9ee853d9855d5000000/search/packages?'
+      )
     }).fail(function(responseObject){
       console.log("error :(");
       console.log(responseObject);
     });
+
   })
 
 });
